4c227b01d7efe8505fe9b8818b6e974f11d25393
Valentin Lorentz <vlorentz@softwareheritage.org>
2024-10-22 16:19:59 +0200

    Document IndexKey and generalize its implementations

--- src/types.rs
+++ src/types.rs
@@ -9,12 +9,21 @@ use anyhow::{Context, Result};
 use arrow::array::*;
 use arrow::buffer::BooleanBuffer;
 use arrow::datatypes::*;
+use parquet::data_type::AsBytes;
 use parquet::arrow::arrow_reader::statistics::StatisticsConverter;
 use parquet::file::metadata::RowGroupMetaData;
 use parquet::file::metadata::{ParquetColumnIndex, ParquetOffsetIndex};
 
+/// Trait of types that can be used to select rows with `parquet_aramid`.
+///
+/// This implies support support for (in call order):
+///
+/// * checking file-level Elias-Fano indexes, using `as_ef_key` (optional)
+/// * checking row group statistics, in [`check_column_chunk`] (optional)
+/// * checking Bloom Filter, implemented through [`AsBytes`] (required)
+/// * checking the page index, in [`check_page_index`] (optional)
 pub trait IndexKey:
-    parquet::data_type::AsBytes + Hash + Eq + Clone + Send + Sync + 'static
+    AsBytes + Clone + Send + Sync + 'static
 {
     /// Returns this key as a value in the file-level Elias-Fano index, if it supports it.
     fn as_ef_key(&self) -> Option<usize>;
@@ -59,11 +68,12 @@ impl<const N: usize> std::ops::Deref for FixedSizeBinary<N> {
     }
 }
 
-impl<const N: usize> parquet::data_type::AsBytes for FixedSizeBinary<N> {
+impl<const N: usize> AsBytes for FixedSizeBinary<N> {
     fn as_bytes(&self) -> &[u8] {
         &self.0
     }
 }
+
 impl<const N: usize> IndexKey for FixedSizeBinary<N> {
     #[inline(always)]
     fn as_ef_key(&self) -> Option<usize> {
@@ -92,106 +102,142 @@ impl<const N: usize> IndexKey for FixedSizeBinary<N> {
     }
 }
 
-impl IndexKey for u64 {
-    #[inline(always)]
-    fn as_ef_key(&self) -> Option<usize> {
-        usize::try_from(*self).ok()
-    }
+macro_rules! impl_type {
+    ($arrow_type:ty, $rust_type:ty) => {
+        impl IndexKey for $rust_type {
+            #[inline(always)]
+            fn as_ef_key(&self) -> Option<usize> {
+                usize::try_from(*self).ok()
+            }
 
-    fn check_column_chunk(
-        keys: &[Self],
-        statistics_converter: &StatisticsConverter,
-        row_groups_metadata: &[RowGroupMetaData],
-    ) -> Result<Option<BooleanBuffer>> {
-        let min_key = keys
-            .iter()
-            .min()
-            .cloned()
-            .context("check_column_chunk got empty set of keys")?;
-        let max_key = keys
-            .iter()
-            .max()
-            .cloned()
-            .context("check_column_chunk got empty set of keys")?;
+            fn check_column_chunk(
+                keys: &[Self],
+                statistics_converter: &StatisticsConverter,
+                row_groups_metadata: &[RowGroupMetaData],
+            ) -> Result<Option<BooleanBuffer>> {
+                let min_key = keys
+                    .iter()
+                    .min()
+                    .cloned()
+                    .context("check_column_chunk got empty set of keys")?;
+                let max_key = keys
+                    .iter()
+                    .max()
+                    .cloned()
+                    .context("check_column_chunk got empty set of keys")?;
 
-        let row_group_mins = statistics_converter
-            .row_group_mins(row_groups_metadata)
-            .context("Could not get row group statistics")?;
-        let row_group_maxes = statistics_converter
-            .row_group_maxes(row_groups_metadata)
-            .context("Could not get row group statistics")?;
-        if row_group_mins.nulls().is_some() || row_group_maxes.nulls().is_some() {
-            unimplemented!("missing column statistics for u64 column chunk")
+                let row_group_mins = statistics_converter
+                    .row_group_mins(row_groups_metadata)
+                    .context("Could not get row group statistics")?;
+                let row_group_maxes = statistics_converter
+                    .row_group_maxes(row_groups_metadata)
+                    .context("Could not get row group statistics")?;
+                if row_group_mins.nulls().is_some() || row_group_maxes.nulls().is_some() {
+                    unimplemented!("missing column statistics for u64 column chunk")
+                }
+                Ok(Some(
+                    arrow::compute::and(
+                        // Discard row groups whose smallest value is greater than the largest key
+                        &BooleanArray::from_unary(
+                            row_group_mins
+                                .as_primitive_opt::<$arrow_type>()
+                                .with_context(|| {
+                                    format!(
+                                        "Could not interpret statistics as {} array",
+                                        std::any::type_name::<$arrow_type>()
+                                    )
+                                })?,
+                            |row_group_min| row_group_min <= max_key,
+                        ),
+                        // Discard row groups whose largest value is less than the smallest key
+                        &BooleanArray::from_unary(
+                            row_group_maxes
+                                .as_primitive_opt::<$arrow_type>()
+                                .with_context(|| {
+                                    format!(
+                                        "Could not interpret statistics as {} array",
+                                        std::any::type_name::<$arrow_type>()
+                                    )
+                                })?,
+                            |row_group_max| row_group_max >= min_key,
+                        ),
+                    )
+                    .context("Could not build boolean array")?
+                    .into_parts()
+                    .0,
+                ))
+            }
+            fn check_page_index<'a, I: IntoIterator<Item = &'a usize> + Copy>(
+                keys: &[Self],
+                statistics_converter: &StatisticsConverter<'a>,
+                column_page_index: &ParquetColumnIndex,
+                column_offset_index: &ParquetOffsetIndex,
+                row_group_indices: I,
+            ) -> Result<Option<BooleanBuffer>> {
+                let min_key = keys
+                    .iter()
+                    .min()
+                    .cloned()
+                    .context("check_page_index got empty set of keys")?;
+                let max_key = keys
+                    .iter()
+                    .max()
+                    .cloned()
+                    .context("check_page_index got empty set of keys")?;
+
+                let data_page_mins = statistics_converter
+                    .data_page_mins(column_page_index, column_offset_index, row_group_indices)
+                    .context("Could not get row group statistics")?;
+                let data_page_maxes = statistics_converter
+                    .data_page_maxes(column_page_index, column_offset_index, row_group_indices)
+                    .context("Could not get row group statistics")?;
+                if data_page_mins.nulls().is_some() || data_page_maxes.nulls().is_some() {
+                    unimplemented!("missing column statistics for u64 page")
+                }
+                Ok(Some(
+                    arrow::compute::and(
+                        // Discard row groups whose smallest value is greater than the largest key
+                        &BooleanArray::from_unary(
+                            data_page_mins
+                                .as_primitive_opt::<$arrow_type>()
+                                .with_context(|| {
+                                    format!(
+                                        "Could not interpret statistics as {} array",
+                                        std::any::type_name::<$arrow_type>()
+                                    )
+                                })?,
+                            |data_page_min| data_page_min <= max_key,
+                        ),
+                        // Discard row groups whose largest value is less than the smallest key
+                        &BooleanArray::from_unary(
+                            data_page_maxes
+                                .as_primitive_opt::<$arrow_type>()
+                                .with_context(|| {
+                                    format!(
+                                        "Could not interpret statistics as {} array",
+                                        std::any::type_name::<$arrow_type>()
+                                    )
+                                })?,
+                            |data_page_max| data_page_max >= min_key,
+                        ),
+                    )
+                    .context("Could not build boolean array")?
+                    .into_parts()
+                    .0,
+                ))
+            }
         }
-        Ok(Some(
-            arrow::compute::and(
-                // Discard row groups whose smallest value is greater than the largest key
-                &BooleanArray::from_unary(
-                    row_group_mins
-                        .as_primitive_opt::<UInt64Type>()
-                        .context("Could not interpret statistics as UInt64Array")?,
-                    |row_group_min| row_group_min <= max_key,
-                ),
-                // Discard row groups whose largest value is less than the smallest key
-                &BooleanArray::from_unary(
-                    row_group_maxes
-                        .as_primitive_opt::<UInt64Type>()
-                        .context("Could not interpret statistics as UInt64Array")?,
-                    |row_group_max| row_group_max >= min_key,
-                ),
-            )
-            .context("Could not build boolean array")?
-            .into_parts()
-            .0,
-        ))
-    }
-    fn check_page_index<'a, I: IntoIterator<Item = &'a usize> + Copy>(
-        keys: &[Self],
-        statistics_converter: &StatisticsConverter<'a>,
-        column_page_index: &ParquetColumnIndex,
-        column_offset_index: &ParquetOffsetIndex,
-        row_group_indices: I,
-    ) -> Result<Option<BooleanBuffer>> {
-        let min_key = keys
-            .iter()
-            .min()
-            .cloned()
-            .context("check_page_index got empty set of keys")?;
-        let max_key = keys
-            .iter()
-            .max()
-            .cloned()
-            .context("check_page_index got empty set of keys")?;
-
-        let data_page_mins = statistics_converter
-            .data_page_mins(column_page_index, column_offset_index, row_group_indices)
-            .context("Could not get row group statistics")?;
-        let data_page_maxes = statistics_converter
-            .data_page_maxes(column_page_index, column_offset_index, row_group_indices)
-            .context("Could not get row group statistics")?;
-        if data_page_mins.nulls().is_some() || data_page_maxes.nulls().is_some() {
-            unimplemented!("missing column statistics for u64 page")
-        }
-        Ok(Some(
-            arrow::compute::and(
-                // Discard row groups whose smallest value is greater than the largest key
-                &BooleanArray::from_unary(
-                    data_page_mins
-                        .as_primitive_opt::<UInt64Type>()
-                        .context("Could not interpret statistics as UInt64Array")?,
-                    |data_page_min| data_page_min <= max_key,
-                ),
-                // Discard row groups whose largest value is less than the smallest key
-                &BooleanArray::from_unary(
-                    data_page_maxes
-                        .as_primitive_opt::<UInt64Type>()
-                        .context("Could not interpret statistics as UInt64Array")?,
-                    |data_page_max| data_page_max >= min_key,
-                ),
-            )
-            .context("Could not build boolean array")?
-            .into_parts()
-            .0,
-        ))
-    }
+    };
 }
+
+impl_type!(UInt64Type, u64);
+impl_type!(UInt32Type, u32);
+impl_type!(UInt16Type, u16);
+impl_type!(UInt8Type, u8);
+
+impl_type!(Int64Type, i64);
+impl_type!(Int32Type, i32);
+impl_type!(Int16Type, i16);
+impl_type!(Int8Type, i8);
+
+// TODO: impl for all the date and time types
