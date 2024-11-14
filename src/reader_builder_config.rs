e5eaa6e9818c4ba53dfab7688c86bb386b4d4533
Valentin Lorentz <vlorentz@softwareheritage.org>
2024-11-14 17:31:57 +0100

    Add FilterPrimitiveConfigurator
    It will be used in unit tests

--- src/reader_builder_config.rs
+++ src/reader_builder_config.rs
@@ -11,6 +11,7 @@ use arrow::datatypes::*;
 use parquet::arrow::arrow_reader::{ArrowPredicateFn, RowFilter};
 use parquet::arrow::async_reader::AsyncFileReader;
 use parquet::arrow::{ParquetRecordBatchStreamBuilder, ProjectionMask};
+use rdst::RadixSort;
 
 use crate::types::FixedSizeBinary;
 use crate::ReaderBuilderConfigurator;
@@ -26,6 +27,79 @@ impl ReaderBuilderConfigurator for NoopConfigurator {
         reader_builder: ParquetRecordBatchStreamBuilder<R>,
     ) -> Result<ParquetRecordBatchStreamBuilder<R>> {
         Ok(reader_builder)
+    }
+}
+
+/// A [`ReaderBuilderConfigurator`] that filters out rows whose value for a given
+/// [primitive](ArrowPrimitiveType) column is not in the given set of allowed values
+pub struct FilterPrimitiveConfigurator<K: ArrowPrimitiveType<Native: Ord> + Send + Sync + 'static> {
+    column_name: &'static str,
+    keys: Arc<Vec<K::Native>>,
+}
+
+impl<K: ArrowPrimitiveType<Native: Ord> + Send + Sync + 'static> FilterPrimitiveConfigurator<K> {
+    pub fn new(column_name: &'static str, mut keys: Vec<K::Native>) -> Self
+    where
+        K::Native: rdst::RadixKey,
+    {
+        keys.radix_sort_unstable();
+        Self::with_sorted_keys(column_name, Arc::new(keys))
+    }
+
+    /// Less efficient implementation of [`Self::new`] for types that don't implement
+    /// [`rdst::RadixKey`]
+    pub fn slow_new(column_name: &'static str, mut keys: Vec<K::Native>) -> Self {
+        keys.sort_unstable();
+        Self::with_sorted_keys(column_name, Arc::new(keys))
+    }
+
+    /// Same as [`Self::new`] but assumes keys are already sorted
+    pub fn with_sorted_keys(column_name: &'static str, keys: Arc<Vec<K::Native>>) -> Self {
+        Self { column_name, keys }
+    }
+}
+
+impl<K: ArrowPrimitiveType<Native: Ord> + Send + Sync + 'static> ReaderBuilderConfigurator
+    for FilterPrimitiveConfigurator<K>
+{
+    fn configure<R: AsyncFileReader>(
+        &self,
+        reader_builder: ParquetRecordBatchStreamBuilder<R>,
+    ) -> Result<ParquetRecordBatchStreamBuilder<R>> {
+        let (column_idx, column) = reader_builder
+            .schema()
+            .column_with_name(self.column_name)
+            .with_context(|| format!("No column names {}", self.column_name))?;
+        ensure!(
+            *column.data_type() == K::DATA_TYPE,
+            "Expected primitive type {} for column {}, got {:?}",
+            K::DATA_TYPE,
+            self.column_name,
+            column.data_type()
+        );
+        let needles = Arc::clone(&self.keys);
+        let row_filter = RowFilter::new(vec![Box::new(ArrowPredicateFn::new(
+            // Only read the column we need for filtering
+            ProjectionMask::roots(reader_builder.parquet_schema(), [column_idx]),
+            move |batch| {
+                let mut matches =
+                    arrow::array::builder::BooleanBufferBuilder::new(batch.num_rows());
+                let haystack = batch
+                    .column(column_idx)
+                    .as_primitive_opt::<K>()
+                    .unwrap_or_else(|| {
+                        panic!("key column is not a primitive array of {}", K::DATA_TYPE)
+                    });
+                for key in haystack {
+                    // Can't panic because we check the schema before applying this row
+                    // filter
+                    let key: K::Native = key.expect("key column contains a null");
+                    matches.append(needles.binary_search(&key).is_ok());
+                }
+                Ok(arrow::array::BooleanArray::new(matches.finish(), None))
+            },
+        ))]);
+        Ok(reader_builder.with_row_filter(row_filter))
     }
 }
 
@@ -78,14 +152,13 @@ impl<const BINARY_SIZE: usize> ReaderBuilderConfigurator
             // Only read the column we need for filtering
             ProjectionMask::roots(reader_builder.parquet_schema(), [column_idx]),
             move |batch| {
-                // TODO: check 'type' column
                 let mut matches =
                     arrow::array::builder::BooleanBufferBuilder::new(batch.num_rows());
-                for key in batch
+                let haystack = batch
                     .column(column_idx)
                     .as_fixed_size_binary_opt()
-                    .expect("key column is not a FixedSizeBinaryArray")
-                {
+                    .expect("key column is not a FixedSizeBinaryArray");
+                for key in haystack {
                     // Can't panic because we check the schema before applying this row
                     // filter
                     let key: [u8; BINARY_SIZE] = key
